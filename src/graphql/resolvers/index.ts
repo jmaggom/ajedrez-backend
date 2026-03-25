368c5efedca4c37ab329f09df58b23333fac9b55
Joaquin Maguilla <joaquinmaguilla@outlook.es>
2026-03-25 10:38:00 +0100

    feat: The resolver and schema merge files have been updated.

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,7 +1,12 @@
 import { authResolvers } from "../../features/auth/auth.resolver";
+import { userResolvers } from "../../features/user/user.resolver";
 
 export const resolvers = {
+    Query: {
+        ...userResolvers.Query,
+    },
     Mutation: {
         ...authResolvers.Mutation,
-    }
+        ...userResolvers.Mutation,
+    },
 }
--- src/graphql/schema/index.ts
+++ src/graphql/schema/index.ts
@@ -1,3 +1,4 @@
 import { authTypeDefs } from "../../features/auth/auth.schema";
+import { userTypeDefs } from "../../features/user/user.schema";
 
-export const typeDefs = [authTypeDefs];
+export const typeDefs = [authTypeDefs, userTypeDefs];
