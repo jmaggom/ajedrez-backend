c0e4b581204988fc3633508a1940951a8f88d00a
Joaquin Maguilla <joaquinmaguilla@outlook.es>
2026-03-31 14:18:22 +0200

    feat: integrate tournament resolvers and type definitions into GraphQL schema

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,12 +1,15 @@
 import { authResolvers } from "../../features/auth/auth.resolver";
 import { userResolvers } from "../../features/user/user.resolver";
+import { tournamentResolvers } from "../../features/tournament/tournament.resolver";
 
 export const resolvers = {
     Query: {
         ...userResolvers.Query,
+        ...tournamentResolvers.Query,
     },
     Mutation: {
         ...authResolvers.Mutation,
         ...userResolvers.Mutation,
+        ...tournamentResolvers.Mutation,
     },
 }
