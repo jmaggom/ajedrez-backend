e0853818fba9e5997581c0cfa169dd4c5c2fcbbf
Joaquin Maguilla <joaquinmaguilla@outlook.es>
2026-04-09 12:05:30 +0200

    feat: integrate notification resolvers into GraphQL schema for improved user notifications handling

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,15 +1,18 @@
 import { authResolvers } from "../../features/auth/auth.resolver";
 import { userResolvers } from "../../features/user/user.resolver";
 import { tournamentResolvers } from "../../features/tournament/tournament.resolver";
+import { notificationResolvers } from "../../features/notification/notification.resolver";
 
 export const resolvers = {
     Query: {
         ...userResolvers.Query,
         ...tournamentResolvers.Query,
+        ...notificationResolvers.Query,
     },
     Mutation: {
         ...authResolvers.Mutation,
         ...userResolvers.Mutation,
         ...tournamentResolvers.Mutation,
+        ...notificationResolvers.Mutation,
     },
 }
