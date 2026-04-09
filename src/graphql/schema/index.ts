114e59c2441ef556dfd336108b4d39f370b67c7a
Joaquin Maguilla <joaquinmaguilla@outlook.es>
2026-04-09 12:05:47 +0200

    feat: add notification type definitions to GraphQL schema for enhanced user notifications

--- src/graphql/schema/index.ts
+++ src/graphql/schema/index.ts
@@ -2,5 +2,6 @@ import { authTypeDefs } from "../../features/auth/auth.schema";
 import { userTypeDefs } from "../../features/user/user.schema";
 import { tournamentTypeDefs } from "../../features/tournament/tournament.schema";
 import { clubTypeDefs } from "../../features/club/club.chema";
+import { notificationTypeDefs } from "../../features/notification/notification.schema";
 
-export const typeDefs = [authTypeDefs, userTypeDefs, tournamentTypeDefs, clubTypeDefs];
+export const typeDefs = [authTypeDefs, userTypeDefs, tournamentTypeDefs, clubTypeDefs, notificationTypeDefs];
