012f92e213c4b60933d27333b563b81f10ab4aa6
Joaquin Maguilla <joaquinmaguilla@outlook.es>
2026-04-16 11:18:54 +0200

    feat: integrate club resolvers into GraphQL for querying and mutating club data

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,18 +1,21 @@
 import { authResolvers } from "../../features/auth/auth.resolver";
 import { userResolvers } from "../../features/user/user.resolver";
 import { tournamentResolvers } from "../../features/tournament/tournament.resolver";
+import { clubResolvers } from "../../features/club/club.resolver";
 import { notificationResolvers } from "../../features/notification/notification.resolver";
 
 export const resolvers = {
     Query: {
         ...userResolvers.Query,
         ...tournamentResolvers.Query,
+        ...clubResolvers.Query,
         ...notificationResolvers.Query,
     },
     Mutation: {
         ...authResolvers.Mutation,
         ...userResolvers.Mutation,
         ...tournamentResolvers.Mutation,
+        ...clubResolvers.Mutation,
         ...notificationResolvers.Mutation,
     },
     Tournament: tournamentResolvers.Tournament,
