86fb0de643bc5a607357bdd0abd6891aef084911
Joaquin Maguilla <joaquinmaguilla@outlook.es>
2026-04-16 10:57:09 +0200

    fix: correct import path for club schema in GraphQL type definitions

--- src/graphql/schema/index.ts
+++ src/graphql/schema/index.ts
@@ -1,7 +1,7 @@
 import { authTypeDefs } from "../../features/auth/auth.schema";
 import { userTypeDefs } from "../../features/user/user.schema";
 import { tournamentTypeDefs } from "../../features/tournament/tournament.schema";
-import { clubTypeDefs } from "../../features/club/club.chema";
+import { clubTypeDefs } from "../../features/club/club.schema";
 import { notificationTypeDefs } from "../../features/notification/notification.schema";
 
 export const typeDefs = [authTypeDefs, userTypeDefs, tournamentTypeDefs, clubTypeDefs, notificationTypeDefs];
